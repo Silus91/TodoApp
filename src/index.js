aacf1e0dcc06084b4f73dbb2878c1438650c6844
Dawid <wizard91@o2.pl>
2019-04-01 23:57:42 +0100

    in progress nav

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import TodoList from './components';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <App  path="/members" component={App}/>
+      <TodoList path="/todolist" component={TodoList} />
+    </Switch>
+  </Router>
+  , document.getElementById('root'));
 
 serviceWorker.unregister();
