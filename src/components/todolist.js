28c69766d94e82d250a876990402f68092cc5c61
Dawid <wizard91@o2.pl>
2019-04-05 23:31:32 +0100

    todolist in progress

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default class TodoList extends React.Component{
+  constructor(props){
+    super(props);
+    
+  }
 
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import TodoList from './components';
+import TodoList from './components/todolist';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
